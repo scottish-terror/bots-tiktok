79676cc53870c0fc66e938de23dbef95567dae6c
srv1054 <scott@mclaughlin.cc>
2019-04-10 10:05:13 -0700

    cron build out

--- tiktok.go
+++ tiktok.go
@@ -27,7 +27,7 @@ func main() {
 
 	// Set version number
 	// Major.Features.Bugs-Tweaks & tomls
-	tiktok.Config.Version = "4.0.2-2"
+	tiktok.Config.Version = "4.0.3-1"
 
 	// Grab CLI parameters at launch
 	tiktokOpts, nocron := tiktokmod.Startup(tiktok)
